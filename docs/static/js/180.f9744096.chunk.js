c5265e9f85a06a712aafce49343f54a2e39ef741
pfuchs <patrick@42heilbronn.de>
2023-07-23 08:53:22 +0200

    asd

---
+++ docs/static/js/180.f9744096.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkswipe_to_bribe=self.webpackChunkswipe_to_bribe||[]).push([[180],{9180:function(e,o,t){t.r(o);var n=null;"undefined"!==typeof WebSocket?n=WebSocket:"undefined"!==typeof MozWebSocket?n=MozWebSocket:"undefined"!==typeof t.g?n=t.g.WebSocket||t.g.MozWebSocket:"undefined"!==typeof window?n=window.WebSocket||window.MozWebSocket:"undefined"!==typeof self&&(n=self.WebSocket||self.MozWebSocket),o.default=n}}]);
+//# sourceMappingURL=180.f9744096.chunk.js.map
